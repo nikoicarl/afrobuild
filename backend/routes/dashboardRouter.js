515ed478790f3e65e4338f62eaf5f125fb00d9c3
nikoicarl <cnikoi@logig.tech>
2025-03-19 15:30:07 -0700

    dashboard router updated

--- backend/routes/dashboardRouter.js
+++ backend/routes/dashboardRouter.js
@@ -35,43 +35,23 @@ module.exports = function(start, Database) {
                                     });
                                     if (Array.isArray(userResult)) {
                                         if (userResult.length > 0) {
-                                            const EmployeeModel = new Employee(Database);
                                             const PrivilegeModel = new Privilege(Database, userid);
-                                            const SetupModel = new Setup(Database);
 
-                                            let employResult = await EmployeeModel.preparedFetch({
-                                                sql: 'employeeid = (SELECT employeeid FROM user_employee WHERE userid = ?)',
-                                                columns: [userid]
-                                            });
                                             if (Array.isArray(employResult)) {
-                                                if (employResult === undefined || employResult.length < 0) {
-                                                    employee = [];
-                                                } else {
-                                                    employee = employResult[0];
-                                                }
-            
+                                            
                                                 let privilegeData = (await PrivilegeModel.getPrivileges()).privilegeData;
 
-                                                let setupResult = await SetupModel.preparedFetch({
-                                                    sql: '1',
-                                                    columns: []
+                                                response.render('dashboard', {
+                                                    pageNavigate: queryStr, 
+                                                    userData: userResult[0],
+                                                    privilege: privilegeData
                                                 });
-                                                if (Array.isArray(setupResult)) {
-                                                    response.render('dashboard', {
-                                                        pageNavigate: queryStr, 
-                                                        employData: employee, 
-                                                        userData: userResult[0],
-                                                        privilege: privilegeData,
-                                                        setupData: setupResult.length > 0 ? setupResult[0] : {}
-                                                    });
-                                                } else {
-                                                    console.log('Invalid user ID 3nd');
-                                                    response.render('index', {pageNavigate: {error: 'loginError5'}, setupData: setupResult.length > 0 ? setupResult[0] : {}});
-                                                }
                                             } else {
                                                 console.log('Invalid user ID 2nd');
                                                 response.render('index', {pageNavigate: {error: 'loginError4'}, setupData: {}});
                                             }
+
+                                            
                                         } else {
                                             console.log('Invalid user ID 1st');
                                             response.render('index', {pageNavigate: {error: 'loginError3'}, setupData: {}});
