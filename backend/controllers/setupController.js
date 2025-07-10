7beff52e7f0714f3f8ea30a1fec14527a1e11f5e
nikoicarl <cnikoi@logig.tech>
2025-07-10 12:49:44 -0700

    setup controller changes

--- backend/controllers/setupController.js
+++ backend/controllers/setupController.js
@@ -127,7 +127,7 @@ module.exports = (socket, Database) => {
                 // Insert customer role
                 const customerRoleID = gf.getTimeStamp();
                 const insertCustomerRole = await RoleModel.insertTable([
-                    customerRoleID, 'Customer', 'Customer', gf.getDateTime(), 'active'
+                    customerRoleID, 'Customer', 'Customer', gf.getDateTime(), 'customer'
                 ]);
                 if (!insertCustomerRole.affectedRows) throw new Error('Failed to insert customer role');
 
